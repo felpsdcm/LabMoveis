91aec9539785c362f2cf573d40558c08703adeb3
Adriely Gogora <99200566+AdryGdS@users.noreply.github.com>
2024-09-12 19:17:24 -0300

    Add files via upload

--- app/Styles/Default.tsx
+++ app/Styles/Default.tsx
@@ -1,17 +1,18 @@
 import { StyleSheet } from "react-native"
 
-const style = StyleSheet.create(
+const Style = StyleSheet.create(
     {
-        container:{
+        container: {
             flex: 1,
             justifyContent: 'center',
             alignItems: 'center',
             backgroundColor: '#f5f5f5',
-          },
-          card: {
+            
+        },
+        card: {
             width: 300,
             height: 200,
-            backgroundColor: '#fff',
+            backgroundColor: '#AAAAAA',
             borderRadius: 10,
             elevation: 5, // Para Android
             shadowColor: '#000', // Para iOS
@@ -21,19 +22,21 @@ const style = StyleSheet.create(
             justifyContent: 'center',
             alignItems: 'center',
             padding: 20,
-            marginVertical: 5,
-          },
-          cardText: {
+            marginVertical: 20
+        },
+        cardText: {
             fontSize: 18,
             color: '#333',
-            fontWeight: 'bold',
-          },
-          image:{
+            fontWeight:"bold"
+        },
+        image:{
             width:100,
             height:100,
             objectFit:"contain",
             resizeMode:"contain"
-          }
-        })
-        export default style  
-        
+        }
+
+    }
+)
+
+export default Style
