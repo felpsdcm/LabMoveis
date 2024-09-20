4b58dcaa05cbaf0c48fa0441a4b40e8d92b8874c
Adriely Gogora <99200566+AdryGdS@users.noreply.github.com>
2024-09-20 19:17:02 -0300

    Updates

--- app/Componentes/Adaptadores/ItemProduto/index.tsx
+++ app/Componentes/Adaptadores/ItemProduto/index.tsx
@@ -1,24 +1,39 @@
 import Produto from "@/app/Models/Produto";
 import Style from "@/app/Styles/Default";
+import axios from "axios";
 import React from "react";
 
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, Button, Alert } from "react-native";
 
 interface PropProd{
     produto:Produto,
-
+    aoExcluir?:Function
 }
 
-const ItemProduto:React.FC<PropProd> = ( {produto})=> {
+const ItemProduto:React.FC<PropProd> = ( {produto, aoExcluir})=> {
     
     console.log(produto)
     
+    
+    function Excluir(id: number) {
+        let api = 'https://api-docker-2t8m.onrender.com/api/produtos';
+        axios.delete(`${api}/${id}`) 
+        .then((resp)=>{
+            aoExcluir?.call(null) ;
+            Alert.alert('Produto excluido com sucesso');
+            alert('Produto excluido com sucesso');
+        })
+    }
+
     return (
         <View style={Style.card} >
             <Text style={Style.cardText} >{produto.nome}</Text>
             <Text style={Style.cardText} >{produto.preco}</Text>
+            <Text style={Style.cardText} >{produto.descricao}</Text>
             <Image source={{uri:produto.foto} } 
              style={Style.image}   />
+            <Button title="Excluir" 
+            onPress={()=>{Excluir(produto.id)}}/>  
             
         </View>
     )
