3333dcde27f3696dc21b90e09fdc11bfa0d3d122
Adriely Gogora <99200566+AdryGdS@users.noreply.github.com>
2024-09-21 00:46:44 +0000

    Atualizações de código

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/Componentes/Adaptadores/CadastroProduto` | `/Componentes/Adaptadores/ItemProduto` | `/Componentes/Adaptadores/ListaProdutos` | `/Models/Produto` | `/Styles/Default` | `/_sitemap`;
+      StaticRoutes: `/` | `/Componentes/Adaptadores/CadastroProduto` | `/Componentes/Adaptadores/ItemProduto` | `/Componentes/Adaptadores/ListaProdutos` | `/Models/Produto` | `/Styles/Default` | `/Telas/cadastro` | `/_sitemap`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
